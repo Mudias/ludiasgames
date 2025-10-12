18b793d18b77ea19c378163b9e470b2b897ff7b0
Muhammed <muhammed_basdemir@windowslive.com>
2025-10-12 17:36:20 +0200

    Added localized versions for the hero image, all assets zip, logos zip, steam capsule zip, and font link

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const translations = {
     us: {
         title: "The Sushi Restaurant",
+        heroImg: "SushiRestaurantStoreCapsuleVertical.png",
         fact: "Fact Sheet",
         factBody: `<br><b>Release Date:</b> Coming Soon
                     <br><b>Developer:</b> <a href="https://linktr.ee/LudiasGames" class="linktree" target="_blank"
@@ -21,9 +22,21 @@ const translations = {
             <br>Can you figure out what?</li>
             <ul>
             `,
+        allAssetsHref: "AllAssets.zip",
+        screenshotsHref:"Screenshots.zip",
+        logosHref: "Logos.zip",
+        steamCapsuleHref: "SteamCapsule.zip",
+        fontTitle: "<b>Font(Stick)</b>",
+        fontHref: "https://fonts.google.com/specimen/Stick?query=stick",
+        fontButtonText: "Font (Stick) Open Font License",
+        templatesHref: "Templates.zip",
+        steamCapsuleSrc: "SushiRestaurantStoreCapsuleHeader.png",
+        logoDarkSrc: "LogoDark.png",
+        logoLightSrc: "LogoLight.png",
     },
     jp: {
         title: "寿司レストラン",
+        heroImg: "Localization/Japanese/SteamCapsule/SushiRestaurantStoreCapsuleVertical_jpn.png",
         fact: "",
         factBody: `<br><b>リリース日:</b> 近日登場 
                     <br><b>開発元:</b> <a href="https://linktr.ee/LudiasGames" class="linktree" target="_blank"
@@ -43,9 +56,21 @@ const translations = {
             <br>君はその正体を突き止められるか？</li>
             <ul>
             `,
+        allAssetsHref: "Localization/Japanese/AllAssets_jpn.zip",
+        screenshotsHref:"Localization/Japanese/Screenshots.zip",
+        logosHref: "Localization/Japanese/Logos_jpn.zip",
+        steamCapsuleHref: "Localization/Japanese/SteamCapsule_jpn.zip",
+        templatesHref: "Localization/Japanese/Templates.zip",
+        steamCapsuleSrc: "Localization/Japanese/SteamCapsule/SushiRestaurantStoreCapsuleVertical_jpn.png",
+        fontTitle: "<b>Font(Stick)</b>",
+        fontHref: "https://fonts.google.com/specimen/Stick?query=stick",
+        fontButtonText: "Font (Stick) Open Font License",
+        logoDarkSrc: "Localization/Japanese/Logos/LogoDark_jpn.png",
+        logoLightSrc: "Localization/Japanese/Logos/LogoLight_jpn.png",
     },
     cn: {
         title: "寿司餐厅",
+        heroImg: "Localization/Simplified Chinese/SteamCapsule/SushiRestaurantStoreCapsuleVertical_zho.png",
         fact: "",
         factBody: `<br><b>发行日期:</b> 即将推出 
                     <br><b>开发者:</b> <a href="https://linktr.ee/LudiasGames" class="linktree" target="_blank"
@@ -64,9 +89,21 @@ const translations = {
             <li class="small-text">有什么东西让气氛变得一天比一天诡异。你能找出原因吗？</li>
             <ul>
             `,
+        allAssetsHref: "Localization/Simplified Chinese/AllAssets_zho.zip",
+        screenshotsHref:"Localization/Simplified Chinese/Screenshots.zip",
+        logosHref: "Localization/Simplified Chinese/Logos_zho.zip",
+        steamCapsuleHref: "Localization/Simplified Chinese/SteamCapsule_zho.zip",
+        templatesHref: "Localization/Simplified Chinese/Templates.zip",
+        steamCapsuleSrc: "Localization/Simplified Chinese/SteamCapsule/SushiRestaurantStoreCapsuleVertical_zho.png",
+        fontTitle: "<b>Font(Stick)</b>",
+        fontHref: "https://fonts.google.com/specimen/Stick?query=stick",
+        fontButtonText: "Font (Stick) Open Font License",
+        logoDarkSrc: "Localization/Simplified Chinese/Logos/LogoDark_zho.png",
+        logoLightSrc: "Localization/Simplified Chinese/Logos/LogoLight_zho.png",
     },
     kr: {
         title: "스시 레스토랑",
+        heroImg: "Localization/Korean/SteamCapsule/SushiRestaurantStoreCapsuleVertical_kor.png",
         fact: "",
         factBody: `<br><b>출시일:</b> 출시 예정 
                     <br><b>개발자:</b> <a href="https://linktr.ee/LudiasGames" class="linktree" target="_blank"
@@ -86,9 +123,21 @@ const translations = {
             <br>당신은 진상을 밝힐 수 있을까요?</li>
             <ul>
             `,
+        allAssetsHref: "Localization/Korean/AllAssets_kor.zip",
+        screenshotsHref:"Localization/Korean/Screenshots.zip",
+        logosHref: "Localization/Korean/Logos_kor.zip",
+        steamCapsuleHref: "Localization/Korean/SteamCapsule_kor.zip",
+        templatesHref: "Localization/Korean/Templates.zip",
+        steamCapsuleSrc: "Localization/Korean/SteamCapsule/SushiRestaurantStoreCapsuleVertical_kor.png",
+        fontTitle: "<b>Font(Stick)</b>",
+        fontHref: "https://fonts.google.com/specimen/Stick?query=stick",
+        fontButtonText: "Font (Stick) Open Font License",
+        logoDarkSrc: "Localization/Korean/Logos/LogoDark_kor.png",
+        logoLightSrc: "Localization/Korean/Logos/LogoLight_kor.png",
     },
     tr: {
         title: "Suşi Restoranı",
+        heroImg: "Localization/Turkish/SteamCapsule/SushiRestaurantStoreCapsuleVertical_tr.png",
         fact: "",
         factBody: `<br><b>Çıkış Tarihi:</b> Pek yakında
                     <br><b>Geliştirici:</b> <a href="https://linktr.ee/LudiasGames" class="linktree" target="_blank"
@@ -109,9 +158,21 @@ const translations = {
             <br>ne olduğunu bulabilir misin?</li>
             <ul>
             `,
+        allAssetsHref: "Localization/Turkish/AllAssets_tr.zip",
+        screenshotsHref:"Localization/Turkish/Screenshots.zip",
+        logosHref: "Localization/Turkish/Logos_tr.zip",
+        steamCapsuleHref: "Localization/Turkish/SteamCapsule_tr.zip",
+        templatesHref: "Localization/Turkish/Templates.zip",
+        steamCapsuleSrc: "Localization/Turkish/SteamCapsule/SushiRestaurantStoreCapsuleVertical_tr.png",
+        fontTitle: "<b>Font(Noto Sans)</b>",
+        fontHref: "https://fonts.google.com/noto/specimen/Noto+Sans?query=noto+sans",
+        fontButtonText: "Font (Noto Sans) Open Font License",
+        logoDarkSrc: "Localization/Turkish/Logos/LogoDark_tr.png",
+        logoLightSrc: "Localization/Turkish/Logos/LogoLight_tr.png",
     },
     de: {
         title: "Sushi-Restaurant",
+        heroImg: "Localization/German/SteamCapsule/SushiRestaurantStoreCapsuleVertical_de.png",
         fact: "",
         factBody: `<br><b>Veröffentlichung:</b> Bald verfügbar
                     <br><b>Entwickler:</b> <a href="https://linktr.ee/LudiasGames" class="linktree" target="_blank"
@@ -131,12 +192,24 @@ const translations = {
             <br>Kannst du herausfinden, was es ist?</li>
             <ul>
             `,
+        allAssetsHref: "Localization/German/AllAssets_de.zip",
+        screenshotsHref:"Localization/German/Screenshots.zip",
+        logosHref: "Localization/German/Logos_de.zip",
+        steamCapsuleHref: "Localization/German/SteamCapsule_de.zip",
+        templatesHref: "Localization/German/Templates.zip",
+        steamCapsuleSrc: "Localization/German/SteamCapsule/SushiRestaurantStoreCapsuleVertical_de.png",
+        fontTitle: "<b>Font(Stick)</b>",
+        fontHref: "https://fonts.google.com/specimen/Stick?query=stick",
+        fontButtonText: "Font (Stick) Open Font License",
+        logoDarkSrc: "Localization/German/Logos/LogoDark_de.png",
+        logoLightSrc: "Localization/German/Logos/LogoLight_de.png"
     },
 };
 
 let currentLang = "us";
 
 const titleEl = document.getElementById("title");
+const heroImgEl = document.getElementById("heroImg");
 const factEl = document.getElementById("fact");
 const factBodyEl = document.getElementById("factBody");
 const aboutThisGameEl = document.getElementById("aboutThisGame");
@@ -144,18 +217,42 @@ const aboutThisGameBodyEl = document.getElementById("aboutThisGameBody");
 const featuresEl = document.getElementById("features");
 const featuresBodyEl = document.getElementById("featuresBody");
 
+const allAssetsEl = document.getElementById("allAssets");
+const screenshotsEl = document.getElementById("screenshots");
+const logosEl = document.getElementById("logos");
+const steamCapsuleEl = document.getElementById("steamCapsule");
+const templatesEl = document.getElementById("templates");
+const steamCapsuleImgEl = document.getElementById("steamCapsuleImg");
+const fontTitleEl = document.getElementById("fontTitle");
+const fontEl = document.getElementById("font");
+const logoDarkEl = document.getElementById("logoDark");
+const logoLightEl = document.getElementById("logoLight");
+
 const selectedLangEl = document.querySelector(".selected-lang");
 
 // Helper to switch languages
 function setLanguage(lang) {
     currentLang = lang;
     titleEl.innerHTML = translations[lang].title;
+    heroImgEl.src = translations[lang].heroImg;
     factEl.innerHTML = translations[lang].fact;
     factBodyEl.innerHTML = translations[lang].factBody;
     aboutThisGameEl.innerHTML = translations[lang].aboutThisGame;
     aboutThisGameBodyEl.innerHTML = translations[lang].aboutThisGameBody;
     featuresEl.innerHTML = translations[lang].features;
     featuresBodyEl.innerHTML = translations[lang].featuresBody;
+
+    allAssetsEl.href = translations[lang].allAssetsHref;
+    screenshotsEl.href = translations[lang].screenshotsHref;
+    logosEl.href = translations[lang].logosHref;
+    steamCapsuleEl.href = translations[lang].steamCapsuleHref;
+    templatesEl.href = translations[lang].templatesHref;
+    steamCapsuleImgEl.src = translations[lang].steamCapsuleSrc;
+    fontTitleEl.innerHTML = translations[lang].fontTitle;
+    fontEl.href = translations[lang].fontHref;
+    fontEl.innerHTML = translations[lang].fontButtonText
+    logoDarkEl.src = translations[lang].logoDarkSrc;
+    logoLightEl.src = translations[lang].logoLightSrc;
 
     selectedLangEl.className = `selected-lang ${lang}`;
     localStorage.setItem("lang", lang); // remember language
@@ -177,10 +274,10 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 function downloadVideo() {
-        const link = document.createElement('a');
-        link.href = 'The Sushi Restaurant Announcement Trailer.mp4';
-        link.download = 'The Sushi Restaurant Announcement Trailer.mp4';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
+    const link = document.createElement('a');
+    link.href = 'The Sushi Restaurant Announcement Trailer.mp4';
+    link.download = 'The Sushi Restaurant Announcement Trailer.mp4';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
